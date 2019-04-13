837c3246eac413783ca3b11e249c6b97dd45261d
araujovicthor <victhor.araujo.41@gmail.com>
2019-04-13 19:25:15 -0300

    Update on slack, listening and statusBid

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ require('dotenv').config();
 const connectionSP = require('./db/connectionSP');
 const connectionUS = require('./db/connectionUS');
 
+
 const getRE = () => new Promise(function(resolve, reject) {
     let filter = "#pub = :status";
     let values = {':status': Boolean(true)};
     let names = {"#pub": "publish"};
+    const listening = [];
     
     let params = {
         TableName: process.env.listingTable,
@@ -28,9 +30,16 @@ const getRE = () => new Promise(function(resolve, reject) {
             console.log('ERROR: ' + err)
             reject(err)
         } else {
-            console.log(data);
-            console.log(Object.keys(data.Items).length);
-            resolve(data);
+            for (var i = 0; i < data.Items.length; i++){
+                let lData = {
+                    "listeningId": data.Items[i].listeningId,
+                    "companyId": data.Items[i].companyId,
+                    "ListingIdImob": data.Items[i].ListingIdImob,
+                    "exhibition": data.Items[i].exhibition
+                }
+                listening.push(lData);
+            }
+            resolve(listening);
         }
     })
 })
@@ -77,11 +86,99 @@ function getBids(listeningId){
                 console.log('ERROR: ' + err)
                 reject(err)
             } else {
-                resolve(Object.keys(data.Items).length);
+                var statusBidAd = false;
+                for (var i = 0; i < data.Items.length; i++){
+                    if (data.Items[i].statusBid === "Evaluate Credit" || data.Items[i].statusBid === "Sign"){
+                        statusBidAd = true;
+                    }
+                }
+                let bid = {qty: Object.keys(data.Items).length, statusBid: statusBidAd};
+                resolve(bid);
             }
         })
     });
 }
 
-getTasks("Test Company", "TC008").then(tasks => console.log("tasks: ",tasks));
-getBids("Pws64vzhNG").then(bids => console.log("bids: ",bids));
+function getCompany(companyId){
+    const params = {
+        TableName: process.env.companyTable,
+        Key:{
+            "companyId": companyId
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        connectionSP.get(params, (err, data) => {
+            if(err) {
+                console.log('ERROR: ' + err)
+                reject(err)
+            } else {
+                resolve(data.Item.companyName);
+            }
+        })
+    });
+}
+
+function slack (listeningId){
+    var request = require("request");
+
+    var options = { 
+        method: 'POST',
+        url:'https://hooks.slack.com/services/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',
+        headers: { 'content-type': 'application/json' },
+        body:{
+            text: 'Um imóvel atingiu 10 visitas sem propostas! <https://portal.ruadois.com.br/imovel/'+listeningId+'|Clique aqui> para ver o imóvel!' 
+        },
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    });
+}
+
+function updateListening (listeningId, exhibition){
+    const params = {
+        TableName: process.env.listingTable,
+        Key:{
+            "listeningId": listeningId
+        },
+        UpdateExpression: "set exhibition = :exhibition",
+        ExpressionAttributeValues:{
+            ":exhibition": exhibition
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        connectionSP.get(params, (err, data) => {
+            if(err) {
+                console.log('ERROR: ' + err)
+                reject(err)
+            } else {
+                resolve(data.Item.companyName);
+            }
+        })
+    });
+}
+
+async function exhibition(){
+    const listening = await getRE();
+    for (var i = 0; i < listening.length; i++){
+        const companyName = await getCompany(listening[i].companyId);
+        const tasks = await getTasks(companyName, listening[i].ListingIdImob);
+        const bids = await getBids(listening[i].listeningId);
+        if (tasks>=10 && bids.qty === 0){
+            slack(listening[i].listeningId)
+        } else if (bids.statusBid === true){
+            let exhibition = 0;
+            updateListening(listening[i].listeningId, exhibition);
+        } else if (tasks<=10 && listening[i].exhibition !== 10){
+            let exhibition = 10;
+            updateListening(listening[i].listeningId, exhibition);
+        } else {
+
+        }
+    }
+}
+
+exhibition();
